e14de88be154a709289f2e4d0974a34fdab52a9b
Dhiraj Patil <dhiraj.4.patil@atos.net>
2022-11-14 17:41:52 +0530

    Changes in Backend

--- src/main/java/com/capstone/ecommerce/controller/UserController.java
+++ src/main/java/com/capstone/ecommerce/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ public class UserController {
 	private UserService uService;
 	
 	@PostMapping("/addUser")
+	@CrossOrigin
 	public User addUser(@RequestBody User user) {
 		return this.uService.addUser(user);
 	}
